77c3f0466c637807b90b088dee4efad302c14f23
Joshua Fajardo <jdfajardo@berkeley.edu>
2023-10-17 23:07:09 -0700

    Write setup_gatys_model, set up some function signatures

--- src/gatys-et-al/util.py
+++ src/gatys-et-al/util.py
@@ -1,1 +1,21 @@
+import tensorflow as tf
+import numpy as np
+import matplotlib as plt
 
+
+def transfer_style_to_content(content_image, style_image, model):
+    pass
+
+def setup_model():
+    pass
+
+def setup_gatys_model(pooling="avg"):
+    model = tf.keras.applications.vgg19.VGG19(include_top=False, weights="imagenet", pooling=pooling)
+    model.trainable = False
+    return model, tf.keras.applications.vgg19.preprocess_input
+
+def calc_gram_matrix(X):
+    pass
+
+def load_image(image_path):
+    pass
