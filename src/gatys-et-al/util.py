29e31e667a7d02c27cbf1d964798f47d324d17be
Joshua Fajardo <jdfajardo@berkeley.edu>
2023-10-19 00:10:28 -0700

    Write transfer_style_to_content, add vgg19_deprocess

--- src/gatys-et-al/util.py
+++ src/gatys-et-al/util.py
@@ -1,24 +1,34 @@
 import tensorflow as tf
+import tensorflow_io as tfio
 import numpy as np
 import matplotlib as plt
 
 
-def transfer_style_to_content(content_image, style_image, model, preprocess, initial_image=None):
-    if initial_image == None:
+def transfer_style_to_content(
+        content_image, style_image, initial_image=None, num_epochs=1000, learning_rate=0.01,
+        alpha=10e-3, beta=1):
+    model, preprocess, deprocess = setup_gatys_model()
+
+    if initial_image is None:
         preprocessed_noise = preprocess(create_white_noise_image(content_image.shape))
-        initial_image = tf.Variable(preprocessed_noise)
-    
+        generated_image = tf.Variable(preprocessed_noise)
+    else:
+        generated_image = initial_image
 
+    optimizer = tf.keras.optimizers.experimental.SGD(learning_rate=learning_rate)
 
+    for i in range(num_epochs):
+        with tf.GradientTape() as tape:
+            loss = calc_total_loss(generated_image, content_image, style_image, alpha, beta)
+        gradient = tape.gradient(loss, generated_image)
+        optimizer.apply_gradients([gradient, generated_image])
 
-def setup_model():
-    pass
-
+    return deprocess(generated_image)
 
 def setup_gatys_model(pooling="avg"):
     model = tf.keras.applications.vgg19.VGG19(include_top=False, weights="imagenet", pooling=pooling)
     model.trainable = False
-    return model, tf.keras.applications.vgg19.preprocess_input
+    return model, tf.keras.applications.vgg19.preprocess_input, vgg19_deprocess
 
 
 def calc_total_loss(generated_image, content_image, style_image, alpha, beta):
@@ -43,3 +53,8 @@ def load_image(image_path):
 
 def create_white_noise_image(shape):
     return np.random.randn(*shape)
+
+def vgg19_deprocess(image):
+    mean = [[[103.939, 116.779, 123.67]]]  # Source: https://github.com/keras-team/keras-applications/blob/master/keras_applications/imagenet_utils.py#L61C9-L61C42
+    image += mean
+    tfio.experimental.color.bgr_to_rgb(image)
