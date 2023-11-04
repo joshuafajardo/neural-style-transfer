f9cb0111e3d979f1056130769592d45b51b9f824
Joshua Fajardo <jdfajardo@berkeley.edu>
2023-11-04 16:24:01 -0700

    Fill out some functions, add some new ones. Mainly doing losses.

--- src/gatys-et-al/util.py
+++ src/gatys-et-al/util.py
@@ -1,3 +1,11 @@
+# util.py
+
+"""Provides tools for transfering style, following Gatys et al."""
+
+__all__ = ["StyledImageFactory", "load_image"]
+__author__ = "joshuafajardo"
+__version__ = "0.1.0"
+
 import tensorflow as tf
 import tensorflow_io as tfio
 import numpy as np
@@ -19,15 +27,36 @@ class StyledImageFactory():
     def __init__(self, content_image, style_image,
                  content_layers=DEFAULT_CONTENT_LAYERS,
                  style_layers=DEFAULT_STYLE_LAYERS,
-                 content_loss_weight=10e-3, style_loss_weight=1):
-        self.content_image = content_image
-        self.style_image = style_image
+                 style_layer_weights=None,
+                 content_loss_weight=10e-3,
+                 style_loss_weight=1):
+        """Initialize the StyledImageFactory."""
+        self.__setup_model(content_layers, style_layers)
+
+        if style_layer_weights is None:
+            style_layer_weights = [1] * len(style_layers)
+        self.style_layer_weights = tf.convert_to_tensor(style_layer_weights)
+
         self.content_loss_weight = content_loss_weight
         self.style_loss_weight = style_loss_weight
-        self.setup_model(content_layers, style_layers)
+
+        # Set up the target representations.
+        content_model_out = self.model(self.preprocess(content_image))
+        style_model_out = self.model(self.preprocess(style_image))
+
+        content_maps = content_model_out["content_maps"]
+        style_maps = style_model_out["style_maps"]
+
+        self.target_content_reps = self.get_content_reps(content_maps)
+        self.target_style_reps = self.get_style_reps(style_maps)
 
     def generate_styled_image(self, initial_image=None, num_epochs=1000,
-                              learning_rate=0.01):
+                              learning_rate=0.01, clip_between_steps=True):
+        """
+        Note: While the original paper doesn't mention anything about
+        clipping the image between optimizer steps, we do it in order
+        to improve the optimization.
+        """  # TODO: Improve the wording
         # Initialize the input to the model.
         if initial_image is None:
             initial_image = self.create_white_noise_image(
@@ -40,7 +69,7 @@ class StyledImageFactory():
         # TODO: run gradient descent on the image.
         return self.deprocess_image(generated_image)
 
-    def setup_model(self, content_layers, style_layers):
+    def __setup_model(self, content_layers, style_layers):
         """
         Sets up an internal "model", where the input is the image to be
         improved, and the output contains the activations (aka
@@ -50,27 +79,64 @@ class StyledImageFactory():
             include_top=False, weights="imagenet", pooling="avg")
         vgg_model.trainable = False
 
-        # "reps" as in "representations"
-        content_reps = [vgg_model.get_layer(layer) for layer in content_layers]
-        style_reps = [vgg_model.get_layer(layer) for layer in style_layers]
+        # "maps" as in "feature maps"
+        content_maps = [vgg_model.get_layer(layer) for layer in content_layers]
+        style_maps = [vgg_model.get_layer(layer) for layer in style_layers]
         outputs = {
-            "content_reps": content_reps,
-            "style_reps": style_reps
+            "content_maps": content_maps,
+            "style_maps": style_maps
         }
 
         self.model = tf.keras.Model([vgg_model.input], outputs)
+    
+    @tf.function()
+    def run_optimizer_step(image):
+        pass
 
+    @tf.function()
     def calc_total_loss():
         pass
 
+    @tf.function()
     def calc_content_loss():
         pass
 
-    def calc_style_loss():
-        pass
+    @tf.function()
+    def calc_style_loss(self, generated_style_reps):
+        num_layers = len(generated_style_reps)
+        contributions = tf.zeros(num_layers)
+        for layer in num_layers:
+            generated_rep = generated_style_reps[layer]
+            target_rep = self.target_style_reps[layer]
+            num_maps, map_size = generated_rep.shape
 
-    def calc_gram_matrix(feature_maps):
-        pass
+            factor = 1 / (4 * (num_maps ** 2) * (map_size ** 2))
+            contributions[layer] = factor * tf.math.reduce_sum(
+                (generated_rep - target_rep) ** 2)
+        return tf.tensordot(self.style_layer_weights, contributions)
+
+    @tf.function()
+    def get_content_reps(self, feature_maps):
+        """
+        Assumes that self.model is already set.
+        """
+        return feature_maps
+
+    @tf.function()
+    def get_style_reps(self, feature_maps):
+        """
+        Assumes that self.model is already set.
+        """
+        reps = []
+        for map in feature_maps:
+            reps.append(self.calc_gram_matrix(map))
+        return reps
+        
+
+    @tf.function()
+    def calc_gram_matrix(feature_map):
+        return tf.matmul(feature_map, feature_map,
+                         transpose_b=True)
 
     def create_white_noise_image(shape):
         """
@@ -84,6 +150,7 @@ class StyledImageFactory():
     
     def deprocess(image):
         image += IMAGENET_MEAN
+        image = np.clip(image, 0, 255)
         return tfio.experimental.color.bgr_to_rgb(image)
 
 def load_image(image_path):
