98f8df861b478a862b1c902e72c631cadf8bfbd5
Joshua Fajardo <jdfajardo@berkeley.edu>
2023-11-03 23:46:35 -0700

    Pivot to an OO approach, using a Factory class.
    Use the factory class to generate the stylized content images.
    
    Additionally, ignore scratchpad.py files.

--- src/gatys-et-al/util.py
+++ src/gatys-et-al/util.py
@@ -4,60 +4,87 @@ import numpy as np
 import matplotlib as plt
 
 
-def transfer_style_to_content(
-        content_image, style_image, initial_image=None, num_epochs=1000, learning_rate=0.01,
-        alpha=10e-3, beta=1):
-    model, preprocess, deprocess = setup_gatys_model()
+IMAGENET_MEAN = [[[103.939, 116.779, 123.67]]]  # Source: https://github.com/keras-team/keras-applications/blob/master/keras_applications/imagenet_utils.py#L61C9-L61C42
 
-    if initial_image is None:
-        preprocessed_noise = preprocess(create_white_noise_image(content_image.shape))
-        generated_image = tf.Variable(preprocessed_noise)
-    else:
-        generated_image = initial_image
+class StyledImageFactory():
+    DEFAULT_CONTENT_LAYERS = ['block4_conv2'] 
+    DEFAULT_STYLE_LAYERS = [
+        'block1_conv1',
+        'block2_conv1',
+        'block3_conv1', 
+        'block4_conv1', 
+        'block5_conv1'
+    ]
 
-    optimizer = tf.keras.optimizers.experimental.SGD(learning_rate=learning_rate)
+    def __init__(self, content_image, style_image,
+                 content_layers=DEFAULT_CONTENT_LAYERS,
+                 style_layers=DEFAULT_STYLE_LAYERS,
+                 content_loss_weight=10e-3, style_loss_weight=1):
+        self.content_image = content_image
+        self.style_image = style_image
+        self.content_loss_weight = content_loss_weight
+        self.style_loss_weight = style_loss_weight
+        self.setup_model(content_layers, style_layers)
 
-    for i in range(num_epochs):
-        with tf.GradientTape() as tape:
-            loss = calc_total_loss(generated_image, content_image, style_image, alpha, beta)
-        gradient = tape.gradient(loss, generated_image)
-        optimizer.apply_gradients([gradient, generated_image])
+    def generate_styled_image(self, initial_image=None, num_epochs=1000,
+                              learning_rate=0.01):
+        # Initialize the input to the model.
+        if initial_image is None:
+            initial_image = self.create_white_noise_image(
+                self.content_image.shape)
+        preprocessed_image = self.preprocess(initial_image)
+        generated_image = tf.Variable(preprocessed_image)
 
-    return deprocess(generated_image)
+        optimizer = tf.keras.optimizers.experimental.SGD(
+            learning_rate=learning_rate)
+        # TODO: run gradient descent on the image.
+        return self.deprocess_image(generated_image)
 
+    def setup_model(self, content_layers, style_layers):
+        """
+        Sets up an internal "model", where the input is the image to be
+        improved, and the output contains the activations (aka
+        representations) of the specified content and style layers.
+        """
+        vgg_model = tf.keras.applications.vgg19.VGG19(
+            include_top=False, weights="imagenet", pooling="avg")
+        vgg_model.trainable = False
 
-def setup_gatys_model(pooling="avg"):
-    model = tf.keras.applications.vgg19.VGG19(include_top=False, weights="imagenet", pooling=pooling)
-    model.trainable = False
-    return model, tf.keras.applications.vgg19.preprocess_input, vgg19_deprocess
+        # "reps" as in "representations"
+        content_reps = [vgg_model.get_layer(layer) for layer in content_layers]
+        style_reps = [vgg_model.get_layer(layer) for layer in style_layers]
+        outputs = {
+            "content_reps": content_reps,
+            "style_reps": style_reps
+        }
 
+        self.model = tf.keras.Model([vgg_model.input], outputs)
 
-def calc_total_loss(generated_image, content_image, style_image, model, alpha, beta):
-    pass
+    def calc_total_loss():
+        pass
 
+    def calc_content_loss():
+        pass
 
-def calc_content_loss(generated_image, content_image, model):
-    pass
+    def calc_style_loss():
+        pass
 
+    def calc_gram_matrix(feature_maps):
+        pass
 
-def calc_style_loss(generated_image, style_image, model, layer_weights):
-    pass
-
-
-def calc_gram_matrix(feature_maps):
-    pass
-
+    def create_white_noise_image(shape):
+        """
+        Create a uniformly random white noise image, with values in range
+        [-128, 127) and with the given shape.
+        """
+        return (np.random.rand(*shape)) * 255 - 128
+    
+    def preprocess(image):
+        return tf.keras.applications.vgg19.preprocess_input(image)
+    
+    def deprocess(image):
+        image += IMAGENET_MEAN
+        return tfio.experimental.color.bgr_to_rgb(image)
 
 def load_image(image_path):
-    pass
-
-
-def create_white_noise_image(shape):
-    """Create a uniformly random white noise image, with values in range [-128, 127) with the given shape."""
-    return (np.random.rand(*shape)) * 255 - 128
-
-def vgg19_deprocess(image):
-    """Does the inverse of the VGG19 preprocessing step on the given image."""
-    mean = [[[103.939, 116.779, 123.67]]]  # Source: https://github.com/keras-team/keras-applications/blob/master/keras_applications/imagenet_utils.py#L61C9-L61C42
-    image += mean
-    return tfio.experimental.color.bgr_to_rgb(image)
+    return tf.keras.utils.load_img(image_path)
