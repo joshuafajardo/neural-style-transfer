Fix kernel matrix computation

src/image_neural_style_transfer/mmd_demo.py
@@ -11,8 +11,8 @@ with tf.device("/GPU:0"):
     style_image = load_image(STYLE_IMAGE_PATH)
     content_image = tf.image.resize(content_image, (768 // 3, 1024 // 3))
 
-    factory = MMDStyledImageFactory(Kernel.LINEAR, content_image, style_image)
-    generated_image, losses = factory.generate_styled_image(num_epochs=100)
+    factory = MMDStyledImageFactory(Kernel.LINEAR, content_image, style_image, balance_factor=10e10)
+    generated_image, losses = factory.generate_styled_image(num_epochs=200)
 
 plt.imshow(tf.cast(generated_image, tf.int32))
 plt.show()

src/image_neural_style_transfer/mmd_nst.py
@@ -125,11 +125,11 @@ class MMDStyledImageFactory(BaseStyledImageFactory):
                     """
                     y_start = partition_num * partition_size
                     y_end = (partition_num + 1) * partition_size
-                    if y_start >= y.shape[0]:
+                    if y_start >= y.shape[1]:
                         return 0
 
                     kernel_calcs = tf.linalg.matmul(
-                        x, y[y_start : y_end], transpose_b=True)
+                        x, y[:, y_start : y_end], transpose_a=True)
                     return tf.math.reduce_sum(kernel_calcs)
                     
                 # The last partition may be smaller.