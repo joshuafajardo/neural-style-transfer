a31e0744b225bce0509547f30168c985c4439f4e
Joshua Fajardo <jdfajardo@berkeley.edu>
2023-12-24 12:50:33 -0800

    flow_to_image

--- src/video_style_transfer/load_mp4s.py
+++ src/video_style_transfer/load_mp4s.py
@@ -58,7 +58,7 @@ def load_and_save_flows(frames, output_dir):
     raft_model = raft_model.eval()
 
     flows = raft_model(start_frames, end_frames)
-    flow_images = torchvision.utils.flows_to_images(flows)
+    flow_images = torchvision.utils.flow_to_image(flows)
     for i, flow_image in enumerate(flow_images):
         frame_num = str(i).zfill(FRAME_COUNT_FILLER)
         output_path = output_dir / f"flow_{frame_num}.png"
